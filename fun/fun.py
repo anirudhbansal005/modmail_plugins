a77a1a13f77bd1ee5480ddbb8e4da801e100c968
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-15 21:43:39 +0530

    Update fun.py

--- fun/fun.py
+++ fun/fun.py
@@ -36,6 +36,8 @@ class FunCommands(commands.Cog):
             embed.set_image(url=random.choice(feedGIF))
             embed.add_field(name="Feed", value=(feed))
             await ctx.send(embed=embed)
+            time.sleep(30)
+            await ctx.message.delete()
         else:
             feedResponse = [ 
             f"{ctx.author.mention} feeds {member.mention}",
