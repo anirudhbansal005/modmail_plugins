d6fcca4e3946ab8ddd9316c815d693b413b5b03e
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-15 22:29:35 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -1,6 +1,6 @@
 import discord
 import typing
-import time
+import asyncio 
 from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
@@ -45,32 +45,32 @@ class Autoreact(commands.Cog):
             return
         if "ohio" in message.content.lower():
             await message.add_reaction("<AXHA:1006778596695744593>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "667378631336525824" in message.content:
             await message.add_reaction("<emoji_21:1033650320636641310>")
             await message.add_reaction("<emoji_22:1033650337053167646>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "735018264651890689" in message.content:
             await message.add_reaction("<emoji_35:1049663858874261545>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "752152316596977704" in message.content:
             await message.add_reaction("<bc_o_carefree:984809639495479316>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "chad" in message.content.lower():
             await message.add_reaction("<bc_z_chadmove:976826985546395678>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "851771484635398175" in message.content:
             await message.add_reaction("<bc_o_worry_foff:979426649470799942>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "860808338442158130" in message.content:
             await message.add_reaction("<bc_z_CosmicChad:1048907381121224815>") 
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
         if "813107139601104917" in message.content: 
             await message.add_reaction("<bc_z_bhaiTuApna:1040891783976075314>")
-            time.sleep(0.5) 
+            await asyncio.sleep(0.5)
         if "789107450942455828" in message.content:
             await message.add_reaction("<emoji_36:1050086330916405268>")
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
     # ----_-_----_-_-------------------
     
     # ----- Sticker Permissions --------
@@ -104,7 +104,7 @@ class Autoreact(commands.Cog):
             if a.filename.endswith(".png") or a.filename.endswith (".apng") or a.filename.endswith (".jpg") or a.filename.endswith(".jpeg"):
                 gc = message.guild.get_channel(906577935874535455)
                 if message.channel == gc and admin not in message.author.roles and manager not in message.author.roles:
-                    time.sleep(30)
+                    await asyncio.sleep(30)
                     await message.delete()
                 else:
                     return
@@ -119,7 +119,7 @@ class Autoreact(commands.Cog):
                 await message.delete()
         if "tenor.com" in message.content or ".gif" in message.content:
             if lv90 in message.author.roles:
-                time.sleep(30)
+                await asyncio.sleep(30)
                 await message.delete()
            
 
