b33dc5410b7d60935b782f177ac76ea8f2b27bc7
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-17 13:09:41 +0530

    Update tempvoice.py

--- tempvoice/tempvoice.py
+++ tempvoice/tempvoice.py
@@ -19,16 +19,17 @@ class TempVoice(commands.Cog):
         if after.channel != None:
             if after.channel.id == 776726741937946644:
                 for guild in self.bot.guilds:
-                    maincategory = discord.utils.get(
-                        guild.categories, id=745514186531930164)
+                    mcategory = discord.utils.get(
+                        guild.categories)
+                    maincategory = member.guild.get_category(745514186531930164)
                     channel2 = await member.guild.create_voice_channel(name=f'канал {member.display_name}', category=maincategory)
                     await channel2.set_permissions(member, connect=True, mute_members=True, manage_channels=True)
                     await member.move_to(channel2)
 
-                def check(x, y, z):
-                    return len(channel2.members) == 0
-                await self.bot.wait_for('voice_state_update', check=check)
-                await channel2.delete()
+                    def check(x, y, z):
+                        return len(channel2.members) == 0
+                        await self.bot.wait_for('voice_state_update', check=check)
+                        await channel2.delete()
 
 
 
