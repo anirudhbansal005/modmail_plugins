5c339cf6a58e4085960db73a28fbd65e42a460ba
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-17 13:05:14 +0530

    Update tempvoice.py

--- tempvoice/tempvoice.py
+++ tempvoice/tempvoice.py
@@ -21,7 +21,7 @@ class TempVoice(commands.Cog):
                 for guild in self.bot.guilds:
                     maincategory = discord.utils.get(
                         guild.categories, id=745514186531930164)
-                    channel2 = await self.bot.guild.create_voice_channel(name=f'канал {member.display_name}', category=maincategory)
+                    channel2 = await member.guild.create_voice_channel(name=f'канал {member.display_name}', category=maincategory)
                     await channel2.set_permissions(member, connect=True, mute_members=True, manage_channels=True)
                     await member.move_to(channel2)
 
