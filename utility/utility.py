a8e6d8401e66a3a0ea29b79ce45bc48e2f18a974
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-19 19:06:09 +0530

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -165,7 +165,6 @@ class UtilityCommands(commands.Cog):
                description = (f"**Bedrock IP :** play.blackclue.in\n"
                        f"**Port :** 19132\n"
                        f"**Java IP :** java.blackclue.in"))
-            author = ctx.author
             await ctx.send(" ".join([ctx.author.mention for author in author]), embed=embed, delete_after=15)
         else:
            embed = discord.Embed(
