58be48860b438de1a1c0c5c27b8ade1a8aa83113
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2022-12-20 00:12:50 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -75,6 +75,12 @@ class Autoreact(commands.Cog):
         if "789107450942455828" in message.content:
             await message.add_reaction("<emoji_36:1050086330916405268>")
             await asyncio.sleep(0.5)
+        if "741567107673751634" in message.content:
+            await message.add_reaction("🤡")
+            await asyncio.sleep(0.5)
+        if "866611673367969842" in message.content:
+            await message.add_reaction("<bc_z_m_eating:881809045114200084>")
+            await asyncio.sleep(0.5)
             return
 
     # ----_-_----_-_-------------------
