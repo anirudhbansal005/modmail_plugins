50ddcea0b1a428810de2932ed31bdc14fe459c06
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-06 19:16:00 +0530

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -4,6 +4,7 @@ from core import checks
 from core.models import PermissionLevel
 import revChatGPT
 import selenium
+import undetected_chromedriver
 from revChatGPT.ChatGPT import Chatbot
 import random
 import asyncio
