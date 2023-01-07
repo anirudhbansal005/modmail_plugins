a5a8969b0d8634be9c77b9886b6cb8909d28b1dc
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-07 17:00:00 +0530

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -229,7 +229,7 @@ class UtilityCommands(commands.Cog):
         doc = await self.db.find_one({"_id": "config"})
         if doc:
             channel_id = doc.get(channel_id)
-            channel = await self.get_channel(channel_id)
+            channel = ctx.guild.get_channel(channel_id)
             await ctx.send(f"Channel: {channel.mention}")
         else:
             await ctx.send("Channel has not been set. Use the `settings` command to set it.")
@@ -243,7 +243,7 @@ class UtilityCommands(commands.Cog):
         doc = await self.db.find_one({"_id": "config"})
         if doc:
             chat_role_id = doc.get(chat_role_id)
-            chat_role = await self.get_role(chat_role_id)
+            chat_role = ctx.guild.get_role(chat_role_id)
             await ctx.send(f"Chat role: {chat_role.mention}")
         else:
             await ctx.send("Chat role has not been set. Use the `settings` command to set it.")
@@ -269,13 +269,15 @@ class UtilityCommands(commands.Cog):
         Shows all the stored channel
         """
         doc = await self.db.find_one({"_id": "config"})
-        if doc:
+        if doc == None:
+            await ctx.send("One of variables are not set")
+        else:
             channel_id = doc.get("channel_id")
             chat_role_id = doc.get("chat_role_id")
             voice_role_id = doc.get("voice_role_id")
-            channel = await self.get_channel(channel_id)
-            chat_role = await self.get_role(chat_role_id)
-            voice_role = await self.get_role(voice_role_id)
+            channel = ctx.guild.get_channel(channel_id)
+            chat_role = ctx.guild.get_role(chat_role_id)
+            voice_role = ctx.guild.get_role(voice_role_id)
             await ctx.send(f"Channel: {channel.mention}\nVoice role: {voice_role.mention}\nChat role: {chat_role.mention}")
 
     @checks.has_permissions(PermissionLevel.OWNER)
@@ -391,12 +393,12 @@ class UtilityCommands(commands.Cog):
         # Add the roles to the members
         for member in chat_members:
             user = await commands.UserConverter().convert(ctx, member)
-            role = await self.get_role(chat_role_id)
+            role = ctx.guild.get_role(chat_role_id)
             if role:
                 await user.add_roles(role, reason="Active chat member",expires_in=timedelta(days=7))
         for member in voice_members:
             user = await commands.UserConverter().convert(ctx, member)
-            role = self.bot.guild.get_role(voice_role_id)
+            role = ctx.guild.get_role(voice_role_id)
             if role:
                 await user.add_roles(role, reason="Active voice member",expires_in=timedelta(days=7))
 
@@ -409,7 +411,7 @@ class UtilityCommands(commands.Cog):
         embed = discord.Embed(title="Active Members", description=description, color=0x00FF00)
 
         # Send the embed message to the channel
-        channel = await self.get_channel(channel_id)
+        channel = ctx.guild.get_channel(channel_id)
         if channel:
             await channel.send(embed=embed)
         else:
