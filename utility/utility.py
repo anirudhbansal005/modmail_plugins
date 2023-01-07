5eb4e3d3067a44ff8f6ca7ca65e7519679aa4052
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-07 18:19:49 +0530

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -399,10 +399,10 @@ class UtilityCommands(commands.Cog):
 
         # Add the roles to the specified members
         for member in chat_members:
-            user = ctx.guild.get_member(member)
+            user = commands.MemberConverter().convert(ctx, member)
             await user.add_roles(chat_role, reason="Added as an active member (chat)")
         for member in voice_members:
-            user = ctx.guild.get_member(member)
+            user = commands.MemberConverter().convert(ctx, member)
             await user.add_roles(voice_role, reason="Added as an active member (voice)")
             
        # Build and send the embed message
