3c7b904ddcca6e72404409a2c31ec1e93fe66727
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-13 22:26:02 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -152,13 +152,16 @@ class Autoreact(commands.Cog):
     async def on_message(self, message):
         if message.content.startswith("!ban") and message.reply is None:
             if any(role.permissions.administrator or role.permissions.ban_members for role in message.author.roles):
-                if message.mentions:
-                    user = message.mentions[0]
-                    reason = message.content.split(" ")[2:]
-                    reason = " ".join(reason)
-                    await user.send(f"You have been banned from the server for the following reason: {reason}\nIf you think this was a mistake, Appeal at - https://forms.gle/tUA9R44tVimMFb8n6 ")
-                    confirmation = await message.channel.send(f"{user.mention} has been banned. Reason: {reason}")
-                    await confirmation.delete(delay=10)
+                if not message.content.startswith(">"):
+                    if message.mentions:
+                        user = message.mentions[0]
+                        reason = message.content.split(" ")[2:]
+                        reason = " ".join(reason)
+                        await user.send(f"You have been banned from the server for the following reason: {reason}\nIf you think this was a mistake, Appeal at - https://forms.gle/tUA9R44tVimMFb8n6 ")
+                        confirmation = await message.channel.send(f"{user.mention} has been banned. Reason: {reason}")
+                        await confirmation.delete(delay=10)
+                    else:
+                        await message.add_reaction('❌')
                 else:
                     await message.add_reaction('❌')
             else:
