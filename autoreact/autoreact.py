a9a12950e820d8c6b1eff0b420154137af623f09
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-14 13:15:14 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -149,18 +149,15 @@ class Autoreact(commands.Cog):
 
    #-------------------------                         
 
-        if message.content.startswith("!ban"):
+        if message.content.startswith("!ban") and message.jump_url is None:
             if any(role.permissions.administrator or role.permissions.ban_members for role in message.author.roles):
-                if message.jump_url is None:
-                    if message.mentions:
-                        user = message.mentions[0]
-                        reason = message.content.split(" ")[2:]
-                        reason = " ".join(reason)
-                        await user.send(f"You have been banned from the server for the following reason: {reason}\nIf you think this was a mistake, Appeal at - https://forms.gle/tUA9R44tVimMFb8n6 ")
-                        confirmation = await message.channel.send(f"{user.mention} has been banned. Reason: {reason}")
-                        await confirmation.delete(delay=10)
-                    else:
-                        await message.add_reaction('❌')
+                if message.mentions:
+                    user = message.mentions[0]
+                    reason = message.content.split(" ")[2:]
+                    reason = " ".join(reason)
+                    await user.send(f"You have been banned from the server for the following reason: {reason}\nIf you think this was a mistake, Appeal at - https://forms.gle/tUA9R44tVimMFb8n6 ")
+                    confirmation = await message.channel.send(f"{user.mention} has been banned. Reason: {reason}")
+                    await confirmation.delete(delay=10)
                 else:
                     await message.add_reaction('❌')
             else:
