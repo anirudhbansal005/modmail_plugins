e5fb7b5cf61eb08225f5ae22df5dd1c74c23d8af
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-01-23 15:08:05 +0530

    Update mcbridge.py

--- mcbridge/mcbridge.py
+++ mcbridge/mcbridge.py
@@ -17,19 +17,19 @@ class mcBridge(commands.Cog):
         self.bot = bot
         self.coll = bot.plugin_db.get_partition(self)
 
-    staff_members = [,]
+    staff_members = [667378631336525824]
 
     @commands.Cog.listener()
     async def on_message(self, message):
-    if message.channel.id == 1054033775673217165:
-        if "warn" in message.content:
-            if message.author.id in staff_members:
-                match = re.search(r"\[\w+\s\d+:\d+:\d+\sINFO\s\]\s(.+?)\sgot\swarned\sby\sServer\sfor:\s(.+?)$", message.content)
-                if match:
-                    username = match.group(1)
-                    reason = match.group(2)
-                    channel = self.bot.get_channel(1045997888502759505)
-                    await channel.send(f"{message.author.name} issued a warning to {username} for: {reason}")
+        if message.channel.id == 1054033775673217165:
+            if "warn" in message.content:
+                if message.author.id in staff_members:
+                    match = re.search(r"\[\w+\s\d+:\d+:\d+\sINFO\s\]\s(.+?)\sgot\swarned\sby\sServer\sfor:\s(.+?)$", message.content)
+                    if match:
+                        username = match.group(1)
+                        reason = match.group(2)
+                        channel = self.bot.get_channel(1045997888502759505)
+                        await channel.send(f"{message.author.name} issued a warning to {username} for: {reason}")
 
 async def setup(bot):
     await bot.add_cog(mcBridge(bot))
