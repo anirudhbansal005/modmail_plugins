c954f15b3544f2b4d8c40258c2e95fb986295780
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-02-19 10:22:44 +0530

    Update tempvoice.py

--- tempvoice/tempvoice.py
+++ tempvoice/tempvoice.py
@@ -15,24 +15,69 @@ class TempVoice(commands.Cog):
         self.bot = bot
         self.coll = bot.plugin_db.get_partition(self)
 
+  #  @commands.Cog.listener()
+  #  async def on_voice_state_update(self, member, before, after):
+   #     if after.channel != None:
+   #         if after.channel.id == 1052439150026047589:
+   ##             for guild in self.bot.guilds:
+    #                category = discord.utils.get(
+    #                    member.guild.categories, id=906587511109865522)
+      #              channel2 = await member.guild.create_voice_channel(name=f'{member.display_name}', category=category)
+       #             await channel2.set_permissions(member, connect=True, manage_channels=True)
+       #             await channel2.edit(user_limit=2)
+       #             await member.move_to(channel2)
+#
+          #         def check(x, y, z):
+        #                return len(channel2.members) == 0
+        #            await self.bot.wait_for('voice_state_update', check=check)
+        #            await channel2.delete()
+              #      return
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if after.channel != None:
+        if after.channel is not None:
             if after.channel.id == 1052439150026047589:
                 for guild in self.bot.guilds:
-                    category = discord.utils.get(
-                        member.guild.categories, id=906587511109865522)
+                    category = discord.utils.get(member.guild.categories, id=906587511109865522)
                     channel2 = await member.guild.create_voice_channel(name=f'{member.display_name}', category=category)
                     await channel2.set_permissions(member, connect=True, manage_channels=True)
                     await channel2.edit(user_limit=2)
                     await member.move_to(channel2)
 
+                    # Send message to the created channel
+                    message = await channel2.send(f"Hey there, {member.mention}! You can modify your temp channel by clicking on the buttons below.")
+                    # Add the button callback function to the bot's wait_for method
+                    self.bot.add_listener(button_callback, "on_component_interaction")
+ 
+
+                    # Define the button row and add the buttons
+                    button_row = discord.ui.ButtonRow(
+                    discord.ui.Button(label="Increase User Limit", custom_id="increase_limit"),
+                    discord.ui.Button(label="Decrease User Limit", custom_id="decrease_limit"),
+                    discord.ui.Button(label="Change Channel Name", custom_id="change_name")
+                           )
+
+                    # Send the button row
+                    await message.edit(components=[button_row])
+                    # Define the buttons and button callback function
+                    async def button_callback(interaction: discord.Interaction):
+                        if interaction.custom_id == "increase_limit":
+                            await channel2.edit(user_limit=channel2.user_limit + 1)
+                            await interaction.response.edit_message(content=f"{member.mention}, the user limit of this channel has been increased to {channel2.user_limit}.")
+                        elif interaction.custom_id == "decrease_limit":
+                           await channel2.edit(user_limit=channel2.user_limit - 1)
+                           await interaction.response.edit_message(content=f"{member.mention}, the user limit of this channel has been decreased to {channel2.user_limit}.")
+                        elif interaction.custom_id == "change_name":
+                           await channel2.edit(name="New Channel Name")
+                           await interaction.response.edit_message(content=f"{member.mention}, the name of this channel has been changed to 'New Channel Name'.")
+   
+
                     def check(x, y, z):
                         return len(channel2.members) == 0
+
                     await self.bot.wait_for('voice_state_update', check=check)
                     await channel2.delete()
-                    return
-
+                        return
 
 async def setup(bot):
     await bot.add_cog(TempVoice(bot))
