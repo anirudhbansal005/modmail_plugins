2a68668afd914bcbc345d0d03cb050142a85e4a6
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-02-19 14:24:54 +0530

    Update tempvoice.py

--- tempvoice/tempvoice.py
+++ tempvoice/tempvoice.py
@@ -23,12 +23,13 @@ class TV(commands.Cog):
     async def on_voice_state_update(self, member, before, after):
         if after.channel is not None:
             if after.channel.id == 1052439150026047589:
-                category = discord.utils.get(member.guild.categories, id=906587511109865522)
-                channel2 = await member.guild.create_voice_channel(name=f'{member.display_name}', category=category)
-                await channel2.set_permissions(member, connect=True, manage_channels=True)
-                await channel2.edit(user_limit=2)
-                await member.move_to(channel2)
-
+                for guild in self.bot.guilds:
+                    category = discord.utils.get(member.guild.categories, id=906587511109865522)
+                    channel2 = await member.guild.create_voice_channel(name=f'{member.display_name}', category=category)
+                    await channel2.set_permissions(member, connect=True, manage_channels=True)
+                    await channel2.edit(user_limit=2)
+                    await member.move_to(channel2)
+ 
                 # Send message to the created channel
                 message = await channel2.send(f"Hey there, {member.mention}! You can modify your temp channel by clicking on the buttons below.")
 
@@ -54,12 +55,12 @@ class TV(commands.Cog):
                 await message.edit(view=button_row)
 
                 # Wait for the voice channel to be empty before deleting it
-                def check(x, y, z):
-                    return len(channel2.members) == 0
+                    def check(x, y, z):
+                        return len(channel2.members) == 0
 
-                await self.bot.wait_for('voice_state_update', check=check)
-                await channel2.delete()
-                return
+                    await self.bot.wait_for('voice_state_update', check=check)
+                    await channel2.delete()
+                    return
 
   #  @commands.Cog.listener()
   #  async def on_voice_state_update(self, member, before, after):
@@ -73,7 +74,7 @@ class TV(commands.Cog):
        #             await channel2.edit(user_limit=2)
        #             await member.move_to(channel2)
 #
-          #         def check(x, y, z):
+          #          def check(x, y, z):
         #                return len(channel2.members) == 0
         #            await self.bot.wait_for('voice_state_update', check=check)
         #            await channel2.delete()
