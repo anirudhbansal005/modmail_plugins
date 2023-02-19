120cebb5acec85de6367d62cc38080a098b0d561
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-02-19 14:58:45 +0530

    Update tempvoice.py

--- tempvoice/tempvoice.py
+++ tempvoice/tempvoice.py
@@ -15,6 +15,30 @@ class TempVoice(commands.Cog):
         self.bot = bot
         self.coll = bot.plugin_db.get_partition(self)
 
+    class TempVoiceView(discord.ui.View):
+        def __init__(self, member):
+            super().__init__()
+            self.member = member
+
+    @discord.ui.button(label='Increase User Limit', custom_id='increase_limit')
+    async def increase_limit(self, button: discord.ui.Button, interaction: discord.Interaction):
+        channel = interaction.channel
+        await channel.edit(user_limit=channel.user_limit + 1)
+        await interaction.response.edit_message(content=f"{self.member.mention}, the user limit of this channel has been increased to {channel.user_limit}.")
+
+    @discord.ui.button(label='Decrease User Limit', custom_id='decrease_limit')
+    async def decrease_limit(self, button: discord.ui.Button, interaction: discord.Interaction):
+        channel = interaction.channel
+        await channel.edit(user_limit=channel.user_limit - 1)
+        await interaction.response.edit_message(content=f"{self.member.mention}, the user limit of this channel has been decreased to {channel.user_limit}.")
+
+    @discord.ui.button(label='Change Channel Name', custom_id='change_name')
+    async def change_name(self, button: discord.ui.Button, interaction: discord.Interaction):
+        channel = interaction.channel
+        await channel.edit(name='New Channel Name')
+        await interaction.response.edit_message(content=f"{self.member.mention}, the name of this channel has been changed to 'New Channel Name'.")
+
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if after.channel != None:
@@ -30,26 +54,8 @@ class TempVoice(commands.Cog):
                 # Send message to the created channel
                     message = await channel2.send(f"Hey there, {member.mention}! You can modify your temp channel by clicking on the buttons below.")
 
-                # Define the button callback function
-                    async def button_callback(interaction: discord.Interaction):
-                        if interaction.custom_id == "increase_limit":
-                            await channel2.edit(user_limit=channel2.user_limit + 1)
-                            await interaction.response.edit_message(content=f"{member.mention}, the user limit of this channel has been increased to {channel2.user_limit}.")
-                        elif interaction.custom_id == "decrease_limit":
-                            await channel2.edit(user_limit=channel2.user_limit - 1)
-                            await interaction.response.edit_message(content=f"{member.mention}, the user limit of this channel has been decreased to {channel2.user_limit}.")
-                        elif interaction.custom_id == "change_name":
-                            await channel2.edit(name="New Channel Name")
-                            await interaction.response.edit_message(content=f"{member.mention}, the name of this channel has been changed to 'New Channel Name'.")
-
-                # Define the button row and add the buttons
-                    button_row = View()
-                    button_row.add_item(Button(label="Increase User Limit", custom_id="increase_limit"))
-                    button_row.add_item(Button(label="Decrease User Limit", custom_id="decrease_limit"))
-                    button_row.add_item(Button(label="Change Channel Name", custom_id="change_name"))
-
-                # Send the button row
-                    await message.edit(view=button_row)
+                    view = TempVoiceView(member)
+                    await message.edit(view=view)
 
                 # Wait for the voice channel to be empty before deleting it
                     def check(x, y, z):
