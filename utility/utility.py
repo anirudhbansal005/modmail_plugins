d70146561222cf43f65eead1a754eda58070f8ae
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2023-04-02 10:43:54 +0530

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -18,48 +18,45 @@ class UtilityCommands(commands.Cog):
 
    
 
-#    @checks.has_permissions(PermissionLevel.MODERATOR)
-#    @commands.command()
-#    async def ranklog(self, ctx, member: discord.Member, gamertag = None, purchase = None , amount = None):
-#        """
-#        logs smp ranks
-#        """
- #       if member == None:
- #           await ctx.send_help(ctx.command)
-#        channel = member.guild.get_channel(960482007379497040) 
-#        embed = discord.Embed(title="Rank Purchase",
-#                color=self.bot.main_color,
- #               description =(
- #                   f"Discord Tag : {member.mention}\n"
- #                    f"Discord User ID : {member.id}\n"
- #                    f"Gamer Tag : {gamertag}\n"
- #                    f"Amount : {amount}\n"
- #                    f"Purchase type : {purchase}\n")
- #               )
-  #      embed.set_footer(text="Utility Plugin v1.10")
-#
-#        await channel.send(embed=embed) 
-#        amethyst = "amethyst" 
-#        ruby = "ruby"
- #       obsidian = "obsidian"
- #       carneline = "carneline"
- #       epidote = "epidote"
-  #      amazonite = "amazonite"
- #       smprank = member.guild.get_role(1003196550220099654)       
- #       if purchase == amethyst.lower():                 
- #           purchase = member.guild.get_role(1034474426902794321)
- #           await member.add_roles(purchase)
- #           await member.add_roles(smprank)
- #           embed = discord.Embed(
- #                   description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
- #           embed2 = discord.Embed(
- #                    description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
- #           await channel.send(embed=embed)
-  #          await channel.send(embed=embed2)
- #           await asyncio.sleep(2592000)
- #           await member.remove_roles(purchase)
-#            await member.remove_roles(smprank)
-#        elif purchase == ruby.lower():                 
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    @commands.command()
+    async def ranklog(self, ctx, member: discord.Member, gamertag = None, purchase = None , amount = None):
+        """
+        logs smp ranks
+        """
+        if member == None:
+            await ctx.send_help(ctx.command)
+        channel = member.guild.get_channel(960482007379497040) 
+        embed = discord.Embed(title="Rank Purchase",
+                color=self.bot.main_color,
+                description =(
+                     f"**Discord Tag:** {member.mention}\n"
+                     f"**Discord User ID:** {member.id}\n"
+                     f"**Gamer Tag:** {gamertag}\n"
+                     f"**Amount:** {amount}\n"
+                     f"**Purchase type:** {purchase}\n")
+                )
+        embed.set_footer(text="Utility Plugin v1.20")
+        await channel.send(embed=embed) 
+        legend = "legend"
+        hero = "hero"
+        mvp = "mvp"
+        vip = "vip"
+        smprank = member.guild.get_role(1003196550220099654)       
+        if purchase == legend.lower():                 
+            purchase = member.guild.get_role(1034474426902794321)
+            await member.add_roles(purchase)
+            await member.add_roles(smprank)
+            embed = discord.Embed(
+                    description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
+            embed2 = discord.Embed(
+                     description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
+            await channel.send(embed=embed)
+            await channel.send(embed=embed2)
+            await asyncio.sleep(2592000)
+            await member.remove_roles(purchase)
+            await member.remove_roles(smprank)
+ #       elif purchase == hero.lower():                 
 #            purchase = member.guild.get_role(1034474078754570353)
 #            await member.add_roles(purchase)
 #            await member.add_roles(smprank)
@@ -72,32 +69,32 @@ class UtilityCommands(commands.Cog):
  #           await asyncio.sleep(2592000)
  #           await member.remove_roles(purchase)
   #          await member.remove_roles(smprank)
- #       elif purchase == obsidian.lower():                 
-  #          purchase = member.guild.get_role(1034474068998631454)
-  #          await member.add_roles(purchase)
-  ##          await member.add_roles(smprank)            
-  #          embed = discord.Embed(
-  #                  description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
-  ##          embed2 = discord.Embed(
-   #                  description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
-  #          await channel.send(embed=embed)
-    #        await channel.send(embed=embed2)
-   #         await asyncio.sleep(2592000)
-  #          await member.remove_roles(purchase)
-  #$          await member.remove_roles(smprank)            
-  #$      elif purchase == epidote.lower():                 
-  #          purchase = member.guild.get_role(1034474059645329481)
-  #          await member.add_roles(purchase)
-  #          await member.add_roles(smprank)
-   #         embed = discord.Embed(
-   #                 description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
-   #         embed2 = discord.Embed(
-    #                 description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
-    #        await channel.send(embed=embed)
-    #        await channel.send(embed=embed2)
-    #        await asyncio.sleep(2592000)
-     #       await member.remove_roles(purchase)
-     #       await member.remove_roles(smprank)
+        elif purchase == mvp.lower():                 
+            purchase = member.guild.get_role(1034474068998631454)
+            await member.add_roles(purchase)
+            await member.add_roles(smprank)            
+            embed = discord.Embed(
+                    description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
+            embed2 = discord.Embed(
+                    description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
+            await channel.send(embed=embed)
+            await channel.send(embed=embed2)
+            await asyncio.sleep(2592000)
+            await member.remove_roles(purchase)
+            await member.remove_roles(smprank)            
+        elif purchase == mvp.lower():                 
+            purchase = member.guild.get_role(1034474059645329481)
+            await member.add_roles(purchase)
+            await member.add_roles(smprank)
+            embed = discord.Embed(
+                    description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
+            embed2 = discord.Embed(
+                    description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
+            await channel.send(embed=embed)
+            await channel.send(embed=embed2)
+            await asyncio.sleep(2592000)
+            await member.remove_roles(purchase)
+            await member.remove_roles(smprank)
     #    elif purchase == carneline.lower():                 
      #       purchase = member.guild.get_role(1034473986211467271)
       #      await member.add_roles(purchase)
@@ -111,25 +108,25 @@ class UtilityCommands(commands.Cog):
    #         await asyncio.sleep(2592000)
    ##         await member.remove_roles(purchase)
   # $         await member.remove_roles(smprank)
- #  $     elif purchase == amazonite.lower():                 
-  #          purchase = member.guild.get_role(1034473906863607889)
-  #          await member.add_roles(purchase)
-   #         await member.add_roles(smprank)
-   #         embed = discord.Embed(
-   #                 description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
-     #       embed2 = discord.Embed(
-   #                  description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
-   #         await channel.send(embed=embed)
-  #          await channel.send(embed=embed2)
-   #         await asyncio.sleep(2592000)
-   #        await member.remove_roles(purchase)
-   #         await member.remove_roles(smprank)                  
-    #    else:
-   #         embed = discord.Embed(
-   #                 description = (
-    #                   f"Couldn’t add roles to {member.mention} due to incorrect log format!\n"
-   #                    f"Please give them roles manually"))
-   #         await channel.send(embed=embed)
+        elif purchase == vip.lower():                 
+            purchase = member.guild.get_role(1034473906863607889)
+            await member.add_roles(purchase)
+            await member.add_roles(smprank)
+            embed = discord.Embed(
+                    description = (f" Successfully added {purchase.mention} to {member.mention} for 30 days\n"))
+            embed2 = discord.Embed(
+                    description = (f" Successfully added {smprank.mention} to {member.mention} for 30days\n"))
+            await channel.send(embed=embed)
+            await channel.send(embed=embed2)
+            await asyncio.sleep(2592000)
+           await member.remove_roles(purchase)
+            await member.remove_roles(smprank)                  
+        else:
+            embed = discord.Embed(
+                    description = (
+                       f"Couldn’t add roles to {member.mention} due to incorrect log format!\n"
+                       f"Please give them roles manually"))
+            await channel.send(embed=embed)
      
     @checks.has_permissions(PermissionLevel.MODERATOR)       
     @commands.command()
@@ -156,46 +153,46 @@ class UtilityCommands(commands.Cog):
             await ctx.message.delete()
             await ctx.send(f"{member.mention}'s ID is {member.id}")
 
-  #  @checks.has_permissions(PermissionLevel.REGULAR)
-  #  @commands.command()
-  #  async def ip(self, ctx, member:discord.Member = None):
-  #      """
-  #      IP address for Blackclue's SMP server
-   #     """
-   #     if member == None:
-   #         embed = discord.Embed(
-   #           title = "Surviving Sheep SMP IP",
-    #          color=self.bot.error_color,
-   #            description = (f"**Bedrock IP :** play.blackclue.in\n"
-      #                 f"**Port :** 19132\n"
-      #                 f"**Java IP :** java.blackclue.in"))
-      #      await ctx.send(f"{ctx.author.mention}", embed=embed, delete_after=60)
-     #   else:
-     #      embed = discord.Embed(
-     #          title = "Surviving Sheep SMP IP",
-     #          color=self.bot.error_color,
-     #          description = (f"**Bedrock IP :** play.blackclue.in\n"
-     #                  f" **Port :** 19132\n"
-     #                  f" **Java IP :** java.blackclue.in"))
-     #      await ctx.send(f"{member.mention}", embed=embed ,delete_after=60)
-     #   await ctx.message.delete()
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.command()
+    async def ip(self, ctx, member:discord.Member = None):
+        """
+        IP address for Blackclue's SMP server
+        """
+        if member == None:
+            embed = discord.Embed(
+              title = "Surviving Sheep SMP IP",
+              color=self.bot.error_color,
+               description = (f"**Bedrock IP :** play.blackclue.in\n"
+                       f"**Port :** 19132\n"
+                       f"**Java IP :** java.blackclue.in"))
+            await ctx.send(f"{ctx.author.mention}", embed=embed, delete_after=60)
+        else:
+           embed = discord.Embed(
+               title = "Surviving Sheep SMP IP",
+               color=self.bot.error_color,
+               description = (f"**Bedrock IP :** play.blackclue.in\n"
+                       f"**Port :** 19132\n"
+                       f"**Java IP :** java.blackclue.in"))
+           await ctx.send(f"{member.mention}", embed=embed ,delete_after=60)
+        await ctx.message.delete()
  
-  #  @checks.has_permissions(PermissionLevel.REGULAR)
-   # @commands.command()
-   # async def vote(self, ctx, member:discord.Member=None):
-    #    if member == None:
-    #        embed = discord.Embed(
-    #           title = "Surviving Sheep SMP Vote Link",
-      #         color = self.bot.error_color,
-      #         description = (f"[Click Here](https://minecraftpocket-servers.com/server/119868/vote/) to vote for smp server!"))
-      #      await ctx.send(f"{ctx.author.mention}\n", embed=embed, delete_after=60)
-     #   else:
-     #       embed = discord.Embed(
-      #         title = "Surviving Sheep SMP Vote Link",
-      #         color = self.bot.error_color,
-      #         description = (f"[Click Here](https://minecraftpocket-servers.com/server/119868/vote/) to vote for smp server!"))
-       #     await ctx.send(f"{member.mention}\n", embed=embed, delete_after=60)
-     #   await ctx.message.delete()
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.command()
+    async def vote(self, ctx, member:discord.Member=None):
+        if member == None:
+            embed = discord.Embed(
+               title = "Surviving Sheep SMP Vote Link",
+               color = self.bot.error_color,
+               description = (f"[Click Here](https://minecraftpocket-servers.com/server/119868/vote/) to vote for smp server!"))
+            await ctx.send(f"{ctx.author.mention}\n", embed=embed, delete_after=60)
+        else:
+            embed = discord.Embed(
+               title = "Surviving Sheep SMP Vote Link",
+               color = self.bot.error_color,
+               description = (f"[Click Here](https://minecraftpocket-servers.com/server/119868/vote/) to vote for smp server!"))
+            await ctx.send(f"{member.mention}\n", embed=embed, delete_after=60)
+        await ctx.message.delete()
 
     @commands.command()
     async def nuke(self, message):
